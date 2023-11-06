a913cc89a621b5198be42c1cb78ec5ccd88d24e3
Yuvaraj Nagi <40654321+yuvi55@users.noreply.github.com>
2023-11-05 23:02:33 -0500

    Login and Signup forms created

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,54 @@
+import { Button } from "./../../components/ui/button.jsx";
+import { Input } from "./../../components/ui/input.jsx";
+
+import React, { useState } from "react";
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here
+    console.log(formData); // For demonstration: log form data on submission
+  };
+
   return (
-    <>
+    <div>
       <h1>Login</h1>
-    </>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <Input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <Input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
   );
 }
 
