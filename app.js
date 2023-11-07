efeb25285a91c6581d173ad8995b9b01cf8f1a64
Dhruv Vaghela <dhruvvaghela17@gmail.com>
2023-11-06 21:02:28 -0500

    added express server and dummy dataset

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import users from "./data/users.js";
 import express from "express";
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
 
 async function main() {
   //  feel free to test any function
