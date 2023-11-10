53761d27c846f5586cc01d092e7a35036da970e7
Dhruv Vaghela <dhruvvaghela17@gmail.com>
2023-11-09 22:54:47 -0500

    fix(seed): minor issues

--- seed.js
+++ seed.js
@@ -1,13 +1,19 @@
-import res from "./data/restaurants.json";
-import { restuarants as resRef } from "./config/mongoCollections";
+import res from "./data/restaurants.json" assert { type: "json" };
+import { closeConnection, dbConnection } from "./config/mongoConnection.js";
+import { restuarants as resRef } from "./config/mongoCollections.js";
 
 const seed = async () => {
   // Your code to seed the database goes here
+  const db = await dbConnection();
+  db.dropDatabase();
+
   const resCollection = await resRef();
   const insertInfo = await resCollection.insertMany(res);
   if (insertInfo.insertedCount === 0) throw "Could not add restaurants";
-  console.log("Successfully added restaurants");
+  closeConnection();
   return true;
 };
+
+await seed();
 
 export default seed;
