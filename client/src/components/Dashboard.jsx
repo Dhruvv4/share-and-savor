0c6f88d8ea2d623600080aae998d54c297c5301b
Yuvaraj Nagi <40654321+yuvi55@users.noreply.github.com>
2023-11-26 20:21:06 -0500

    UI changes and minor fixes

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,30 +2,36 @@ import { Link } from "react-router-dom";
 import restaurants from "./../../../data/NJData.json";
 import { Button } from "./ui/button";
 import Search from "./Search";
+
 function Dashboard() {
   return (
     <>
-      <h1 className="text-center">
-        Save Meals from your favorite restaurants !
+      <h1 className="text-3xl font-semibold text-center my-8">
+        Save Meals from your favorite restaurants!
       </h1>
-      <Search></Search>
-      <div className="container mx-52 p-4 my-5 items-center">
-        <div
-          className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 
-        "
-        >
+      <Search />
+      <div className="container mx-auto p-4 my-5">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
           {restaurants.map((res) => (
             <div
               key={res.id}
-              className="border-2 border-solid border-gray-300 p-4"
+              className="border border-solid border-gray-300 rounded p-6 transition transform hover:shadow-lg"
             >
-              <img src="/Image_not_available.png" alt="No image found" />
-              <h1>Name: {res.Restaurant}</h1>
-              <h2>Cuisine: {res.Cuisine}</h2>
-              <h3>Rating Count: {res.Rating_Count}</h3>
-              <h3>Rating: {res.Star_Count}</h3>
-              <h4>Operation hours: Today between {}</h4>
-              <h4>Location: {res.Address}</h4>
+              <img
+                src="/Image_not_available.png"
+                alt="No image found"
+                className="mb-4 rounded-md"
+              />
+              <h1 className="text-xl font-semibold mb-2">{res.Restaurant}</h1>
+              <h2 className="text-gray-600 mb-2">Cuisine: {res.Cuisine}</h2>
+              <div className="flex items-center space-x-2 text-gray-600 mb-2">
+                <span>Rating Count: {res.Rating_Count}</span>
+                <span>Rating: {res.Star_Count}</span>
+              </div>
+              <h4 className="text-gray-600 mb-2">
+                Operation hours: Today between {}
+              </h4>
+              <h4 className="text-gray-600">Location: {res.Address}</h4>
             </div>
           ))}
         </div>
