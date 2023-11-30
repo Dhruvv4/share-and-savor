cf5cd7d3d795b694affb61d2a17902aca9ad41eb
Dhruv Vaghela <dhruvvaghela17@gmail.com>
2023-11-29 21:47:01 -0500

    refactor(profile): minor fix

--- client/src/components/ProfileDropdown.jsx
+++ client/src/components/ProfileDropdown.jsx
@@ -11,7 +11,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import ThemeToggle from "@/components/ThemeToggle";
-import { useAuth } from "@/context/appContext";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "@/features/userSlice";
 import { Link } from "react-router-dom";
